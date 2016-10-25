9b4e4df295f497f90a2a4a11bc4935d60db117d5
Stefan Fruhner <stefan.fruhner@googlemail.com>
2016-10-25 21:24:53 +0200

    Fix the weather data download.

--- pebble/src/js/pebble-js-app.js
+++ pebble/src/js/pebble-js-app.js
@@ -10,7 +10,8 @@ var xhrRequest = function (url, type, callback) {
 function locationSuccess(pos) {
   // Construct URL
   var url = "http://api.openweathermap.org/data/2.5/weather?lat=" +
-      pos.coords.latitude + "&lon=" + pos.coords.longitude;
+      pos.coords.latitude + "&lon=" + pos.coords.longitude +
+      "&appid=2a43f0e8458e4302a205019966303ab3";
 
   // Send request to OpenWeatherMap
   xhrRequest(url, 'GET',
